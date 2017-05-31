29cb010c3f08e38e6ad6f9bf009c88400a3acccb
Tom Revill <tom.revill@gmail.com>
2017-05-31 13:54:58 +1200

    Colours for the World!

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -7,7 +7,10 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
+  two()
+  three()
+  four()
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -23,14 +26,60 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
 
+  // Next, we add an event listener to it:
+  two.addEventListener('mouseenter', makeGreen)
+
+  // Finally, we add one to make the colour white again
+  two.addEventListener('mouseleave', makeWhite)
+}
 // CREATE FUNCTION three HERE
 
+function three () {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
+
+  // Next, we add an event listener to it:
+  three.addEventListener('mouseenter', makeOrange)
+
+  // Finally, we add one to make the colour white again
+  three.addEventListener('mouseleave', makeWhite)
+}
+
 // CREATE FUNCTION four HERE
+
+function four () {
+  // First, we have to find the element:
+  var four = document.getElementById('four')
+
+  // Next, we add an event listener to it:
+  four.addEventListener('mouseenter', makeBlack)
+
+  // Finally, we add one to make the colour white again
+  four.addEventListener('mouseleave', makeWhite)
+}
+
+
+
 
 // Changes the background color of event's target
 function makeBlue (evt) {
   evt.target.style.backgroundColor = 'blue'
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
+}
+
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange'
+}
+
+function makeBlack (evt) {
+  evt.target.style.backgroundColor = 'black'
 }
 
 function makeWhite (evt) {
